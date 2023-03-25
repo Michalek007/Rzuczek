30af472ec827f00e47a470a7702f5c631b7013d2
Michalek007 <nmichal02@op.pl>
2023-03-25 23:42:39 +0100

    Added functions: get_data, save_data

--- rest-api/app/esp32_connection.py
+++ rest-api/app/esp32_connection.py
@@ -1,11 +1,10 @@
 import serial
-import time
 
 # s_write = serial.Serial('COM5', 921600, timeout=0, parity=serial.PARITY_EVEN, rtscts=1)
 s_read = serial.Serial('COM6', 921600, timeout=0, parity=serial.PARITY_EVEN, rtscts=1)
 
-while True:
-    print(s_read.read_all())
-    # print(s_read.read(100))
-    time.sleep(0.01)
-    # s_write.write(b"DUPA")
+
+def get_data():
+    data = s_read.read_all().split('\n')
+    values = {'temp': data[0], 'press': data[0], 'absl': data[0]}
+    return values
--- rest-api/app/periodic_tasks.py
+++ rest-api/app/periodic_tasks.py
@@ -1,21 +1,20 @@
 from flask_apscheduler import APScheduler
-import psutil
-from datetime import datetime
 from app import app
 from database import db, Parameters
+from app.esp32_connection import get_data
 
 scheduler = APScheduler()
 
 
-# @scheduler.task("cron", id="save_performance", minute="*")
-# def save_performance():
-#     parameters = Performance(date=str(datetime.now()), memory_usage=psutil.virtual_memory()[2],
-#                              CPU_usage=psutil.cpu_percent(), disk_usage=psutil.disk_usage('/')[3])
-#     with app.app_context():
-#         db.session.add(parameters)
-#         db.session.commit()
-#     print("Performance saved!")
-#
-#
+@scheduler.task("cron", id="save_data", minute="*")
+def save_data():
+    values = get_data()
+    data = Parameters(temp=values["temp"], press=values["press"], absl=values["absl"])
+    with app.app_context():
+        db.session.add(data)
+        db.session.commit()
+    print("Parameters saved!")
+
+
 scheduler.init_app(app)
 scheduler.start()
